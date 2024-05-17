15f27767d98141735abb9635fb08e54681cbaafd
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-05-17 03:57:14 +0100

    Plotter accounts for speed links

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -20,7 +20,12 @@ def main() -> None:
         lines = [line.rstrip() for line in f]
 
     # Build the config from the experiment
-    Config = namedtuple("Config", ["item_count", "item_size", "seed"])
+    # NOTE: The download and upload values should be change if they are changed in the experiment.
+    Config = namedtuple(
+        "Config",
+        ["item_count", "item_size", "seed", "download", "upload"],
+        defaults=[0, 0, 42, 32_000, 32_000],
+    )
     config_params = (int(p) for p in lines[0].split(" ", 3))
     config = Config(*config_params)
     print(config)
@@ -30,18 +35,28 @@ def main() -> None:
     similarity = np.arange(start, stop + 1, step)
 
     # Create the plot environment
-    fig, ax = plt.subplots(2)
-    fig.suptitle("Synchronization Metrics for GSets")
+    fig, ax = plt.subplots(2, figsize=(8, 8))
+    fig.suptitle("Sync Metrics (with GSets)")
 
-    ax[0].xaxis.set_major_formatter(ticker.PercentFormatter())
-    ax[0].set(xticks=similarity, xlabel="similarity", ylabel="transmitted (bytes)")
+    ax[0].xaxis.set_major_formatter(ticker.PercentFormatter(stop))
+    ax[0].set(
+        title="Transmitted bytes",
+        xlabel="similarity",
+        xmargin=0,
+        ylabel="transmitted (Bytes)",
+    )
 
-    ax[1].xaxis.set_major_formatter(ticker.PercentFormatter())
-    ax[1].set(xticks=similarity, xlabel="similarity", ylabel="time (s)")
+    ax[1].xaxis.set_major_formatter(ticker.PercentFormatter(stop))
+    ax[1].set(
+        title=f"Time to sync \n {config.upload}/{config.download} B/s up/down",
+        xlabel="similarity",
+        xmargin=0,
+        ylabel="time (s)",
+    )
 
     # Plot the size of a replica to give a reference
     replica_size = config.item_count * config.item_size
-    ax[0].axhline(replica_size, color="xkcd:grey", linestyle="-")
+    ax[0].axhline(replica_size, label="replica size", color="xkcd:grey", linestyle="--")
 
     # Data retrieaval from the experiment file
     exchanged_bytes = defaultdict(list)
@@ -57,15 +72,19 @@ def main() -> None:
         hops[proto].append(int(run_params[2]))
 
     for proto, b in exchanged_bytes.items():
-        ax[0].plot(similarity, b, label=proto)
+        ax[0].plot(similarity, b, label=proto, linestyle=":")
 
     for proto, d in durations.items():
-        ax[1].plot(similarity, d, label=proto)
+        ax[1].plot(similarity, d, label=proto, linestyle=":")
 
     for proto, h in hops.items():
         print(f"{proto} avg. {np.mean(h)} hops")
 
+    # Print the labels for each proto before finish
+    ax[0].legend(loc="lower right", fontsize="small")
+    ax[1].legend(fontsize="small")
     fig.tight_layout()
+
     plt.show()
 
 
