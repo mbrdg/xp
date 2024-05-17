942e44bed56c15fa562cca771200a8649bf4bf60
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-05-17 02:38:27 +0100

    Script for plotting experiments

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -1,16 +1,73 @@
+# plotter.py
+# Plots the data gathered from experiements
+
+import argparse
+from collections import defaultdict, namedtuple
+import fileinput
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import ticker
+
 
 def main() -> None:
-    t = np.arange(0.0, 2.0, 0.01)
-    s = 1 + np.sin(2 * np.pi * t)
+    parser = argparse.ArgumentParser(prog="plotter")
+    parser.add_argument("filename", nargs="?")
+    args = parser.parse_args()
 
-    fig, ax = plt.subplots()
-    ax.plot(t, s)
-    ax.set(xlabel='time (s)', ylabel='voltage (mV)')
-    ax.grid()
+    # File reading
+    file = args.filename if args.filename else None
+    with fileinput.input(files=file) as f:
+        lines = [line.rstrip() for line in f]
 
+    # Build the config from the experiment
+    Config = namedtuple("Config", ["item_count", "item_size", "seed"])
+    config_params = (int(p) for p in lines[0].split(" ", 3))
+    config = Config(*config_params)
+    print(config)
+
+    # Similarities
+    start, stop, step = tuple(int(p) for p in lines[1].split(" ", 3))
+    similarity = np.arange(start, stop + 1, step)
+
+    # Create the plot environment
+    fig, ax = plt.subplots(2)
+    fig.suptitle("Synchronization Metrics for GSets")
+
+    ax[0].xaxis.set_major_formatter(ticker.PercentFormatter())
+    ax[0].set(xticks=similarity, xlabel="similarity", ylabel="transmitted (bytes)")
+
+    ax[1].xaxis.set_major_formatter(ticker.PercentFormatter())
+    ax[1].set(xticks=similarity, xlabel="similarity", ylabel="time (s)")
+
+    # Plot the size of a replica to give a reference
+    replica_size = config.item_count * config.item_size
+    ax[0].axhline(replica_size, color="xkcd:grey", linestyle="-")
+
+    # Data retrieaval from the experiment file
+    exchanged_bytes = defaultdict(list)
+    durations = defaultdict(list)
+    hops = defaultdict(list)
+
+    for run in lines[2:]:
+        run_params = run.split(" ", 5)
+
+        proto = run_params[0]
+        exchanged_bytes[proto].append(int(run_params[4]))
+        durations[proto].append(float(run_params[3]))
+        hops[proto].append(int(run_params[2]))
+
+    for proto, b in exchanged_bytes.items():
+        ax[0].plot(similarity, b, label=proto)
+
+    for proto, d in durations.items():
+        ax[1].plot(similarity, d, label=proto)
+
+    for proto, h in hops.items():
+        print(f"{proto} avg. {np.mean(h)} hops")
+
+    fig.tight_layout()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,6 @@ fn populate_replicas(
 ) -> (GSet<String>, GSet<String>) {
     let similar_items = (count as f64 * (similarity as f64 / 100.0)) as usize;
     let diff_items = count - similar_items;
-    eprintln!("similarity: {similarity} ({similar_items}, {diff_items})");
 
     let (mut l, mut r) = (GSet::new(), GSet::new());
     let mut rng = StdRng::seed_from_u64(seed);
@@ -70,34 +69,40 @@ fn print_stats(protocol: &str, similarity: usize, tracker: DefaultTracker) {
             .sum::<usize>(),
     );
 
-    println!("{protocol} {similarity} {hops} {duration:?} {bytes}")
+    println!(
+        "{protocol} {similarity} {hops} {:.3} {bytes}",
+        duration.as_secs_f64()
+    )
 }
 
 fn main() {
     let start = Instant::now();
 
     let (item_count, item_size, seed) = (100_000, 80, random());
-    eprintln!("config: {item_count} {item_size} {seed}");
+    println!("{item_count} {item_size} {seed}");
 
-    for similarity in (0..=100).rev().step_by(5) {
-        let (local, remote) = populate_replicas(item_count, item_size, similarity, seed);
+    let (similarity, step) = (0..=100, 5);
+    println!("{} {} {step}", similarity.start(), similarity.end());
+
+    for s in similarity.rev().step_by(5) {
+        let (local, remote) = populate_replicas(item_count, item_size, s, seed);
 
         let baseline = Baseline::new(local.clone(), remote.clone()).sync();
-        print_stats("baseline", similarity, baseline);
+        print_stats("baseline", s, baseline);
 
         // NOTE: The number of buckets must increase accordingly to the set's size.
         let buckets_1024 = Buckets::<1024>::new(local.clone(), remote.clone()).sync();
-        print_stats("buckets_1024", similarity, buckets_1024);
+        print_stats("buckets<1024>", s, buckets_1024);
 
         let buckets_2048 = Buckets::<2048>::new(local.clone(), remote.clone()).sync();
-        print_stats("buckets_2048", similarity, buckets_2048);
+        print_stats("buckets<2048>", s, buckets_2048);
 
         let buckets_4096 = Buckets::<4096>::new(local.clone(), remote.clone()).sync();
-        print_stats("buckets_4096", similarity, buckets_4096);
+        print_stats("buckets<4096>", s, buckets_4096);
 
         let buckets_8192 = Buckets::<8192>::new(local.clone(), remote.clone()).sync();
-        print_stats("buckets_8192", similarity, buckets_8192);
+        print_stats("buckets<8192>", s, buckets_8192);
     }
 
-    eprintln!("Took {:.3?}", start.elapsed());
+    eprintln!("time elapsed {:.3?}", start.elapsed());
 }
