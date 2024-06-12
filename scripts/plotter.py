29047fead2ace5bf8c781108896fdd24a2fedad8
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-06-12 18:43:57 +0100

    Plotter log reading

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -3,100 +3,101 @@
 
 import argparse
 from collections import defaultdict
-import fileinput
+from io import TextIOWrapper
 from typing import NamedTuple
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 
 
+class Header(NamedTuple):
+    size_of_local: int
+    upload: int
+    size_of_remote: int
+    download: int
+
+
 class Metrics(NamedTuple):
-    transmitted: int
+    state: int
+    metadata: int
     duration: float
 
 
 class Experiment(NamedTuple):
-    data: dict[str, list[Metrics]]
-    avg_size_of_local: int
-    avg_size_of_remote: int
-    download: int
-    upload: int
+    env: Header
+    runs: dict[str, list[Metrics]]
 
 
-similarity = np.arange(0, 101, 10)
+similarities = range(0, 101, 10)
 percent_formatter = ticker.PercentFormatter()
-byte_formatter = ticker.EngFormatter(unit="B", places=2)
+byte_formatter = ticker.EngFormatter(unit="B")
+bit_formatter = ticker.EngFormatter(unit="b")
 
 
-def read_experiment(input: fileinput.FileInput) -> Experiment:
+def read_experiments(f: TextIOWrapper) -> list[Experiment]:
     """
-    Reads experiments from the input source.
-
-    The input is assumed to be formatted with the following columns separated by whitespaces.
-    > protocol | size_of_local | size_of_remote | download | upload | transferred | duration
+    Reads an experiment from the input source.
+    This function assumes that the input is not malformed.
     """
-    data = defaultdict(list[Metrics])
-    sizes_of_local = []
-    sizes_of_remote = []
-    upload = None
-    download = None
+    # Ignore the first empty line
+    _ = f.readline()
 
-    while parts := input.readline().rstrip().split(maxsplit=6):
-        # Read the replica sizes
-        sizes_of_local.append(int(parts[1]))
-        sizes_of_remote.append(int(parts[2]))
+    envs = set()
+    collector = [
+        defaultdict(list[Metrics]),
+        defaultdict(list[Metrics]),
+        defaultdict(list[Metrics]),
+    ]
 
-        # Read values for the links
-        if download is None or upload is None:
-            download = int(parts[3])
-            upload = int(parts[4])
-        assert download == int(parts[3]) and upload == int(parts[4])
+    for _ in similarities:
+        for m in collector:
+            env = Header(*map(int, f.readline().rstrip().split()))
+            envs.add(env)
 
-        # Collect relevant metrics
-        metrics = Metrics(int(parts[5]), float(parts[6]))
-        data[parts[0]].append(metrics)
+            while parts := f.readline().rstrip().split():
+                proto, *metrics = parts
+                state, metadata, duration = (
+                    int(metrics[0]),
+                    int(metrics[1]),
+                    float(metrics[2]),
+                )
+                m[proto].append(Metrics(state, metadata, duration))
 
-    assert download is not None and upload is not None
-    assert all(len(v) == np.size(similarity) for v in data.values())
-
-    avg_size_of_local = int(sum(sizes_of_local) / len(sizes_of_local))
-    avg_size_of_remote = int(sum(sizes_of_remote) / len(sizes_of_remote))
-    return Experiment(data, avg_size_of_local, avg_size_of_remote, download, upload)
+    return [Experiment(*p) for p in zip(envs, collector)]
 
 
-def plot_transmitted(experiment: Experiment):
-    """Produce the plot containing the bytes transferred between similar replicas"""
+def plot_transmitted_data(experiment: Experiment):
+    """Plots the transmitted data over the network for each protocol"""
     _, ax = plt.subplots(layout="constrained")
 
     ax.xaxis.set_major_formatter(percent_formatter)
     ax.yaxis.set_major_formatter(byte_formatter)
-    ax.grid(linestyle="--", linewidth=.5, alpha=.5)
-    ax.set(xlabel="Similarity Ratio", xmargin=0, ylabel="Transmitted (Bytes)")
+    ax.grid(linestyle="--", linewidth=0.5, alpha=0.5)
+    ax.set(xlabel="Similarity", xmargin=0, ylabel="Transmitted (Bytes)")
 
-    for protocol, metrics in experiment.data.items():
-        transmitted = np.array([v.transmitted for v in metrics], dtype=np.uint64)
-        ax.plot(similarity, transmitted, marker="o", label=protocol)
+    for proto, metrics in experiment.runs.items():
+        transmitted = [m.state + m.metadata for m in metrics]
+        ax.plot(similarities, transmitted, marker="o", label=proto)
 
     ax.legend(fontsize="x-small")
     plt.show()
 
 
-def plot_similar_time(experiments: tuple[Experiment, Experiment, Experiment]):
-    """Produce the plot containing the sync times among different bandwidths for similar replicas"""
-    _, axes = plt.subplots(1, 3, figsize=(6.4 * 3, 4.8), layout="constrained")
+def plot_time_to_sync(experiments: list[Experiment]):
+    """Plots the time to sync on different link configurations"""
+    assert len(experiments) == 3
+    _, axes = plt.subplots(ncols=3, figsize=(6.4 * 3, 4.8), layout="constrained")
 
-    for exp, ax in zip(experiments, axes):
-        upload = byte_formatter(exp.upload)
-        download = byte_formatter(exp.download)
-        label = f"Sync Time (s)\n{upload}/s up, {download}/s down"
+    for (env, runs), ax in zip(experiments, axes):
+        up, down = bit_formatter(env.upload), bit_formatter(env.download)
+        ylabel = f"Time to Sync (s)\n{up}/s up, {down}/s down"
 
         ax.xaxis.set_major_formatter(percent_formatter)
-        ax.grid(linestyle="--", linewidth=.5, alpha=.5)
-        ax.set(xlabel="Similarity Ratio", xmargin=0, ylabel=label)
+        ax.grid(linestyle="--", linewidth=0.5, alpha=0.5)
+        ax.set(xlabel="Similarity", xmargin=0, ylabel=ylabel)
 
-        for protocol, metrics in exp.data.items():
-            sync_time = np.array([v.duration for v in metrics], dtype=np.float64)
-            ax.plot(similarity, sync_time, marker="o", label=protocol)
+        for proto, metrics in runs.items():
+            time = [m.duration for m in metrics]
+            ax.plot(similarities, time, marker="o", label=proto)
 
         ax.legend(fontsize="x-small")
 
@@ -106,7 +107,9 @@ def plot_similar_time(experiments: tuple[Experiment, Experiment, Experiment]):
 def main():
     """Script that extracts relevant data from logs and produces the plots for each experiment"""
     parser = argparse.ArgumentParser(prog="plotter")
-    parser.add_argument("filename", nargs="?", default="-")
+    parser.add_argument(
+        "filenames", nargs="*", default=("-"), type=argparse.FileType("r")
+    )
     args = parser.parse_args()
 
     # Set global configs for plotting
@@ -114,21 +117,11 @@ def main():
     plt.rc("font", family="serif")
 
     # File reading
-    with fileinput.input(files=args.filename) as input:
-        # Ignore the the line containing the seed and the next empty line
-        _ = input.readline()
-        _ = input.readline()
+    for file in args.filenames:
+        exps = read_experiments(file)
 
-        # Replicas with similarity
-        gsets_eq = read_experiment(input)
-        assert gsets_eq.upload == gsets_eq.download, gsets_eq
-        gsets_lt = read_experiment(input)
-        assert gsets_lt.upload < gsets_lt.download, gsets_lt
-        gsets_gt = read_experiment(input)
-        assert gsets_gt.upload > gsets_gt.download, gsets_gt
-
-    plot_transmitted(gsets_eq)
-    plot_similar_time((gsets_lt, gsets_eq, gsets_gt))
+        plot_transmitted_data(exps[1])
+        plot_time_to_sync(exps)
 
 
 if __name__ == "__main__":
